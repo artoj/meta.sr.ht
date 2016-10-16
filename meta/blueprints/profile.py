dcb63ec811b23f41f1ec78b8de16302a0e32256a
Drew DeVault <sir@cmpwn.com>
2016-10-16 00:26:44 -0400

    Finish implementing keys page

--- meta/blueprints/profile.py
+++ meta/blueprints/profile.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 from pyotp import TOTP
 from meta.common import loginrequired
 from meta.types import User, EventType, UserAuthFactor, FactorType
-from meta.types import AuditLogEntry, SSHKey
+from meta.types import AuditLogEntry, SSHKey, PGPKey
 from meta.validation import Validation, valid_url
 from meta.email import send_email
 from meta.config import _cfg
@@ -11,6 +11,7 @@ from meta.audit import audit_log
 from meta.qrcode import gen_qr
 from meta.db import db
 import sshpubkeys as ssh
+import pgpy
 import base64
 import os
 
@@ -195,6 +196,60 @@ def ssh_keys_POST():
     key = SSHKey(user, ssh_key, fingerprint, parsed_key.comment)
     db.add(key)
     audit_log(EventType.add_ssh_key, 'Added SSH key {}'.format(fingerprint))
+    db.commit()
+    return redirect("/keys")
+
+@profile.route("/keys/delete-ssh/<key_id>", methods=["POST"])
+@loginrequired
+def ssh_keys_delete(key_id):
+    user = User.query.get(current_user.id)
+    key = SSHKey.query.get(int(key_id))
+    if not key or key.user_id != user.id:
+        abort(404)
+    audit_log(EventType.deleted_ssh_key, 'Deleted SSH key {}'.format(key.fingerprint))
+    db.delete(key)
+    db.commit()
+    return redirect("/keys")
+
+@profile.route("/keys/pgp-keys", methods=["POST"])
+@loginrequired
+def pgp_keys_POST():
+    user = User.query.get(current_user.id)
+    valid = Validation(request)
+
+    pgp_key = valid.require("pgp-key")
+    if valid.ok:
+        try:
+            key = pgpy.PGPKey()
+            key.parse(pgp_key.replace('\r', '').encode('utf-8'))
+        except:
+            valid.error("This is not a valid PGP key", "pgp-key")
+    if valid.ok:
+        valid.expect(PGPKey.query\
+            .filter(PGPKey.user_id == user.id) \
+            .filter(PGPKey.key_id == key.fingerprint)\
+            .count() == 0, "This is a duplicate key", "pgp-key")
+    if not valid.ok:
+        return render_template("keys.html",
+            current_user=user,
+            pgp_key=pgp_key,
+            valid=valid)
+
+    pgp = PGPKey(user, pgp_key, key.fingerprint, key.userids[0].email)
+    db.add(pgp)
+    audit_log(EventType.add_pgp_key, 'Added PGP key {}'.format(key.fingerprint))
+    db.commit()
+    return redirect("/keys")
+
+@profile.route("/keys/delete-pgp/<key_id>", methods=["POST"])
+@loginrequired
+def pgp_keys_delete(key_id):
+    user = User.query.get(current_user.id)
+    key = PGPKey.query.get(int(key_id))
+    if not key or key.user_id != user.id:
+        abort(404)
+    audit_log(EventType.deleted_pgp_key, 'Deleted PGP key {}'.format(key.key_id))
+    db.delete(key)
     db.commit()
     return redirect("/keys")
 
--- meta/types/pgpkey.py
+++ meta/types/pgpkey.py
@@ -6,15 +6,18 @@ from enum import Enum
 class PGPKey(Base):
     __tablename__ = 'pgpkey'
     id = sa.Column(sa.Integer, primary_key=True)
+    created = sa.Column(sa.DateTime)
     user_id = sa.Column(sa.Integer, sa.ForeignKey('user.id'))
     user = sa.orm.relationship('User', backref=sa.orm.backref('pgp_keys'))
     key = sa.Column(sa.String(4096))
     key_id = sa.Column(sa.String(512))
     email = sa.Column(sa.String(256))
 
-    def __init__(self, key, key_id):
+    def __init__(self, user, key, key_id, email):
+        self.user_id = user.id
         self.key = key
         self.key_id = key_id
+        self.email = email
 
     def __repr__(self):
         return '<PGPKey {} {}>'.format(self.id, self.key_id)
