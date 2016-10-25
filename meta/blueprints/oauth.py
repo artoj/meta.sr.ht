6d09edc835f4f25daf40995947608a0632f2d373
Drew DeVault <sir@cmpwn.com>
2016-10-25 07:52:03 -0400

    Implement OAuth client registration

--- meta/blueprints/oauth.py
+++ meta/blueprints/oauth.py
@@ -1,9 +1,59 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, session
+from flask_login import current_user
+from meta.validation import Validation, valid_url
 from meta.common import loginrequired
+from meta.types import OAuthClient, OAuthToken
+from meta.audit import audit_log
+from meta.db import db
 
 oauth = Blueprint('oauth', __name__, template_folder='../../templates')
 
 @oauth.route("/oauth")
 @loginrequired
 def oauth_GET():
-    return render_template("oauth.html")
+    def tokens(client):
+        return OAuthToken.query \
+                .filter(OAuthToken.client_id == client.id).count()
+    return render_template("oauth.html", tokens=tokens)
+
+@oauth.route("/oauth/register")
+@loginrequired
+def oauth_register_GET():
+    return render_template("oauth-register.html")
+
+@oauth.route("/oauth/register", methods=["POST"])
+@loginrequired
+def oauth_register_POST():
+    valid = Validation(request)
+
+    client_name = valid.require("client-name")
+    redirect_uri = valid.optional("redirect-uri")
+
+    valid.expect(not redirect_uri or valid_url(redirect_uri),
+            "Must be a valid HTTP or HTTPS URI", field="redirect-uri")
+
+    if not valid.ok:
+        return render_template("oauth-register.html",
+                client_name=client_name,
+                redirect_uri=redirect_uri,
+                valid=valid)
+
+    client = OAuthClient(current_user, client_name, redirect_uri)
+    secret = client.gen_client_secret()
+    session["client_id"] = client.client_id
+    session["client_secret"] = secret
+    db.add(client)
+    audit_log("register oauth client",
+            "Registered OAuth client {}".format(client.client_id))
+    db.commit()
+    return redirect("/oauth/registered")
+
+@oauth.route("/oauth/registered")
+@loginrequired
+def oauth_registered():
+    client_id = session["client_id"]
+    client_secret = session["client_secret"]
+    del session["client_id"]
+    del session["client_secret"]
+    return render_template("oauth-registered.html",
+            client_id=client_id, client_secret=client_secret)
--- meta/types/oauthclient.py
+++ meta/types/oauthclient.py
@@ -12,20 +12,23 @@ class OAuthClient(Base):
     updated = sa.Column(sa.DateTime, nullable=False)
     user_id = sa.Column(sa.Integer, sa.ForeignKey('user.id'))
     user = sa.orm.relationship('User', backref=sa.orm.backref('oauth_clients'))
+    client_name = sa.Column(sa.Unicode(256), nullable=False)
     client_id = sa.Column(sa.String(16), nullable=False)
-    client_secret_hash = sa.Column(sa.String(32), nullable=False)
+    client_secret_hash = sa.Column(sa.String(128), nullable=False)
     client_secret_partial = sa.Column(sa.String(8), nullable=False)
     redirect_uri = sa.Column(sa.String(256))
 
-    def __init__(self, user):
+    def __init__(self, user, client_name, redirect_uri):
         self.user_id = user.id
+        self.client_name = client_name
+        self.redirect_uri = redirect_uri
         self.gen_client_id()
 
     def gen_client_id(self):
-        self.client_id = binascii.hexlify(os.urandom(8))
+        self.client_id = binascii.hexlify(os.urandom(8)).decode()
 
     def gen_client_secret(self):
         secret = binascii.hexlify(os.urandom(16)).decode()
         self.client_secret_partial = secret[:8]
-        self.client_secret_hash = hashlib.sha512(secret).hexdigest()
+        self.client_secret_hash = hashlib.sha512(secret.encode()).hexdigest()
         return secret
