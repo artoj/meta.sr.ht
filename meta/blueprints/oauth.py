6b41345fdbadc16f71dc08b2e98b9397dba5a726
Drew DeVault <sir@cmpwn.com>
2016-10-25 08:11:24 -0400

    Implement resetting OAuth client secrets

--- meta/blueprints/oauth.py
+++ meta/blueprints/oauth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, abort
 from flask_login import current_user
 from meta.validation import Validation, valid_url
 from meta.common import loginrequired
@@ -42,6 +42,7 @@ def oauth_register_POST():
     secret = client.gen_client_secret()
     session["client_id"] = client.client_id
     session["client_secret"] = secret
+    session["client_event"] = "registered"
     db.add(client)
     audit_log("register oauth client",
             "Registered OAuth client {}".format(client.client_id))
@@ -53,7 +54,26 @@ def oauth_register_POST():
 def oauth_registered():
     client_id = session["client_id"]
     client_secret = session["client_secret"]
+    client_event = session["client_event"]
     del session["client_id"]
     del session["client_secret"]
+    del session["client_event"]
     return render_template("oauth-registered.html",
-            client_id=client_id, client_secret=client_secret)
+            client_id=client_id,
+            client_secret=client_secret,
+            client_event=client_event)
+
+@oauth.route("/oauth/reset-secret/<client_id>", methods=["POST"])
+@loginrequired
+def reset_secret(client_id):
+    client = OAuthClient.query.filter(OAuthClient.client_id == client_id).first()
+    if not client or client.user_id != current_user.id:
+        abort(404)
+    secret = client.gen_client_secret()
+    session["client_id"] = client.client_id
+    session["client_secret"] = secret
+    session["client_event"] = "reset-secret"
+    audit_log("reset client secret",
+            "Reset OAuth client secret for {}".format(client.client_id))
+    db.commit()
+    return redirect("/oauth/registered")
