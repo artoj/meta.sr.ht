4f0703e884f55ecfaebf7db2608a451c5e1a62ec
Drew DeVault <sir@cmpwn.com>
2017-02-05 16:59:50 -0500

    Remove unnecessary function from common.py

--- meta/common.py
+++ meta/common.py
@@ -24,14 +24,3 @@ def adminrequired(f):
             if not current_user.admin:
                 abort(401)
             return f(*args, **kwargs)
-    return wrapper
-
-def optional_arg_decorator(fn):
-    def wrapped_decorator(*args, **kwargs):
-        if len(args) == 1 and callable(args[0]):
-            return fn(args[0])
-        else:
-            def real_decorator(decoratee):
-                return fn(decoratee, *args, **kwargs)
-            return real_decorator
-    return wrapped_decorator
