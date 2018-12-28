7d61857ec6acbd7808fcf826cca6802eb43ed89e
Drew DeVault <sir@cmpwn.com>
2018-12-28 11:06:30 -0500

    Don't email exempt users every billing cycle

--- metasrht/billing.py
+++ metasrht/billing.py
@@ -4,19 +4,26 @@ from srht.config import cfg
 from srht.database import db
 from metasrht.audit import audit_log
 from metasrht.types import User, UserType, PaymentInterval, Invoice
+from enum import Enum
 
 stripe.api_key = cfg("meta.sr.ht::billing", "stripe-secret-key")
 
+class ChargeResult(Enum):
+    success = "success"
+    failed = "failed"
+    cancelled = "failed"
+    account_current = "account_current"
+
 def charge_user(user):
     if user.user_type == UserType.active_free:
-        return True, "Your account is exempt from payment."
+        return ChargeResult.account_current, "Your account is exempt from payment."
     if user.payment_due >= datetime.utcnow():
-        return True, "Your account is current."
+        return ChargeResult.account_current, "Your account is current."
     desc = f"{cfg('sr.ht', 'site-name')} {user.payment_interval.value} payment"
     if user.payment_cents == 0:
         # They cancelled their payment and their current term is up
         user.user_type = UserType.active_non_paying
-        return True, "Your paid service has been cancelled."
+        return ChargeResult.cancelled, "Your paid service has been cancelled."
     try:
         amount = user.payment_cents
         if user.payment_interval == PaymentInterval.yearly:
@@ -32,7 +39,7 @@ def charge_user(user):
     except stripe.error.CardError as e:
         details = e.json_body["error"]["message"]
         user.user_type = UserType.active_delinquent
-        return False, details
+        return ChargeResult.failed, details
     invoice = Invoice()
     invoice.cents = amount
     invoice.user_id = user.id
@@ -49,4 +56,4 @@ def charge_user(user):
         invoice.valid_thru = datetime.utcnow() + timedelta(days=365)
         user.payment_due = invoice.valid_thru
     user.user_type = UserType.active_paying
-    return True, "Your card was successfully charged. Thank you!"
+    return ChargeResult.success, "Your card was successfully charged. Thank you!"
