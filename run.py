d4f8d5960ed28c09b43b1cefe40f1e711e206c8b
Ludovic Chabant <ludovic@chabant.com>
2019-01-11 07:07:53 +0000

    Use new way to run debug/local services

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-from metasrht.app import app
-from srht.config import cfg, cfgi
-
-import os
-
-app.static_folder = os.path.join(os.getcwd(), "static")
-
 if __name__ == '__main__':
-    app.run(host=cfg("meta.sr.ht", "debug-host"),
-            port=cfgi("meta.sr.ht", "debug-port"),
-            debug=True)
+    from srht.debug import run_service
+    from metasrht.app import app
+    run_service(app)
