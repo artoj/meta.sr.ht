cf41fa0ac4ec1b15126624080cbd92a4cb47b139
Drew DeVault <sir@cmpwn.com>
2019-02-13 11:00:52 -0500

    Use celery for webhook delivery

--- metasrht/webhooks.py
+++ metasrht/webhooks.py
@@ -1,6 +1,14 @@
-from srht.webhook import Webhook, Event
+from srht.config import cfg
+from srht.database import DbSession, db
+if not hasattr(db, "session"):
+    # Initialize the database if not already configured (for running daemon)
+    db = DbSession(cfg("meta.sr.ht", "connection-string"))
+    import metasrht.types
+    db.init()
+from srht.webhook import Event
+from srht.webhook.celery import CeleryWebhook, worker
 
-class UserWebhook(Webhook):
+class UserWebhook(CeleryWebhook):
     events = [
         Event("profile:update", "profile:read"),
         Event("ssh-key:add", "keys:read"),
