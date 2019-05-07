1aa6da89159ad9dd53921ffb840bbc6ab27be949
Drew DeVault <sir@cmpwn.com>
2019-05-07 10:37:36 -0400

    Revert "Fix SSH keys via API"
    This reverts commit 0a3e5392eba332e1084de4ac2583160f6611957b.

--- metasrht/blueprints/api/keys.py
+++ metasrht/blueprints/api/keys.py
@@ -6,7 +6,7 @@ keys = Blueprint('api.keys', __name__)
 @keys.route("/api/ssh-key/<path:key_id>")
 def ssh_key_GET(key_id):
     # TODO: parse this and do it properly instead of being a dumb idiot
-    key = SSHKey.query.filter(SSHKey.key.ilike(key_id)).one_or_none()
+    key = SSHKey.query.filter(SSHKey.key.ilike("%"+key_id+"%")).one_or_none()
     if not key:
         abort(404)
     return key.to_dict()
