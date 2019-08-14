6fb79be6498a9e28dab500957f91d1b1f959da6d
Jelle Besseling <jelle@pingiun.com>
2019-08-14 16:36:39 +0200

    Add last_used update internal api
    Add API that can be used by scripts to update the last_used field for a
    base64 ssh key.

--- metasrht/blueprints/api/keys.py
+++ metasrht/blueprints/api/keys.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, abort
+from flask import Blueprint, abort, request
+from srht.crypto import verify_request_signature
+from srht.database import db
 from metasrht.types import SSHKey, PGPKey
+from datetime import datetime
 
 keys = Blueprint('api.keys', __name__)
 
@@ -8,6 +11,16 @@ def ssh_key_GET(key_id):
     key = SSHKey.query.filter(SSHKey.b64_key == key_id).one_or_none()
     if not key:
         abort(404)
+    return key.to_dict()
+
+@keys.route("/api/ssh-key/<path:key_id>", methods=["POST"])
+def ssh_key_PUT(key_id):
+    verify_request_signature(request)
+    key = SSHKey.query.filter(SSHKey.b64_key == key_id).one_or_none()
+    if not key:
+        abort(404)
+    key.last_used = datetime.utcnow()
+    db.session.commit()
     return key.to_dict()
 
 @keys.route("/api/pgp-key/<path:key_id>")
