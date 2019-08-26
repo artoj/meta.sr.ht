9aa2f7f5f9202f51e3bda7b5590f101e2a483f53
Jelle Besseling <jelle@pingiun.com>
2019-08-26 17:30:14 +0200

    Save b64_key and key_type in SSHKey table
    Use a function from the sshpubkeys module to parse the base64 key data
    from the ssh key. This allows the keys api to search the database
    exactly based on the base 64 data.

--- metasrht/types/sshkey.py
+++ metasrht/types/sshkey.py
@@ -11,6 +11,8 @@ class SSHKey(Base):
     user_id = sa.Column(sa.Integer, sa.ForeignKey('user.id'))
     user = sa.orm.relationship('User', backref=sa.orm.backref('ssh_keys'))
     key = sa.Column(sa.String(4096))
+    b64_key = sa.Column(sa.String(4096))
+    key_type = sa.Column(sa.String(256))
     fingerprint = sa.Column(sa.String(512))
     comment = sa.Column(sa.String(256))
     last_used = sa.Column(sa.DateTime)
@@ -38,6 +40,8 @@ class SSHKey(Base):
         if not valid.ok:
             return
         self.key = ssh_key
+        self.b64_key = extract_b64_key(ssh_key)
+        self.key_type = parsed_key.key_type
         self.fingerprint = fingerprint
         self.comment = parsed_key.comment
         db.session.flush()
@@ -70,3 +74,30 @@ class SSHKey(Base):
                 "last_used": self.last_used,
             } if current_token and current_token.user_id == self.user_id else {})
         }
+
+def extract_b64_key(data):
+    """Extract the base64 ssh key portion from an ssh public key.
+
+    This function is adapted from the python-sshpubkeys project[0]. It is
+    copied here because the sshpubkeys module currently has no public API to
+    get the base64 portion of a key. Some error checking in this function has
+    been edited out because this function should only be used when the key
+    has already been parsed by the SSHKey class from the sshpubkeys module
+    (which means this function was already called with error checking).
+
+    [0] https://github.com/ojarva/python-sshpubkeys/blob/9d6289c717a79dd8e49311af647877c95ebc41d3/sshpubkeys/keys.py#L204
+    """
+    # Terribly inefficient way to remove options, but hey, it works.
+    if not data.startswith("ssh-") and not data.startswith("ecdsa-"):
+        quote_open = False
+        for i, character in enumerate(data):
+            if character == '"':  # only double quotes are allowed, no need to care about single quotes
+                quote_open = not quote_open
+            if quote_open:
+                continue
+            if character == " ":
+                # Data begins after the first space
+                data = data[i + 1:]
+                break
+    key_parts = data.strip().split(None, 2)
+    return key_parts[1]
