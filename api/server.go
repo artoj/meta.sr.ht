45f3410d7525462b535c34a319c636ff66833496
Drew DeVault <sir@cmpwn.com>
2020-06-02 11:36:38 -0600

    API: Implement @internal directive

--- api/server.go
+++ api/server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/99designs/gqlgen/graphql"
 	"git.sr.ht/~sircmpwn/gql.sr.ht"
+	"git.sr.ht/~sircmpwn/gql.sr.ht/auth"
 
 	"git.sr.ht/~sircmpwn/meta.sr.ht/api/graph"
 	"git.sr.ht/~sircmpwn/meta.sr.ht/api/graph/api"
@@ -12,6 +17,12 @@ func main() {
 	appConfig := gql.LoadConfig(":5100")
 
 	gqlConfig := api.Config{Resolvers: &graph.Resolver{}}
+	gqlConfig.Directives.Internal = func(ctx context.Context, obj interface{}, next graphql.Resolver) (interface{}, error) {
+		if auth.ForContext(ctx).AuthMethod != auth.AUTH_INTERNAL {
+			return nil, fmt.Errorf("Access denied")
+		}
+		return next(ctx)
+	}
 	schema := api.NewExecutableSchema(gqlConfig)
 
 	router := gql.MakeRouter("meta.sr.ht", appConfig, schema, loaders.Middleware)
