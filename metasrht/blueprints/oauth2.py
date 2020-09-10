75b8fb7ea2a9dae054328b8b53fed5accf60420e
Drew DeVault <sir@cmpwn.com>
2020-09-10 13:21:55 -0400

    Discover scopes from other APIs

--- metasrht/blueprints/oauth2.py
+++ metasrht/blueprints/oauth2.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, render_template
+import requests
+from flask import Blueprint, render_template, request
+from srht.config import config, get_origin
 from srht.oauth import loginrequired
+from srht.validation import Validation
 
 oauth2 = Blueprint('oauth2', __name__)
 
+print("Discovering APIs...")
+access_grants = []
+for s in config:
+    if not s.endswith(".sr.ht"):
+        continue
+    origin = get_origin(s)
+    r = requests.get(f"{origin}/query/api-meta.json")
+    if r.status_code != 200:
+        continue
+    print(f"Found {s}")
+    access_grants.append({
+        "name": s,
+        "scopes": r.json()["scopes"],
+    })
+
 @oauth2.route("/oauth2")
 @loginrequired
-def oauth2_dashboard():
+def dashboard():
     dashboard_query = """
     query {
         oauthGrants {
@@ -20,3 +38,15 @@ def oauth2_dashboard():
     """
     # TODO: Run that query
     return render_template("oauth2-dashboard.html")
+
+@oauth2.route("/oauth2/personal-token")
+@loginrequired
+def personal_token_GET():
+    return render_template("oauth2-personal-token-registration.html",
+            access_grants=access_grants)
+
+@oauth2.route("/oauth2/personal-token", methods=["POST"])
+@loginrequired
+def personal_token_POST():
+    validation = Validation(request)
+    # TODO
