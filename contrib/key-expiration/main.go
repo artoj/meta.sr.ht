582b296e33e5196069544eecc6b124aa79b42705
Conrad Hoffmann <ch@bitfehler.net>
2022-08-22 20:49:11 +0200

    contrib/key-expiration: handle failure cases
    This script must keep going in face of all errors. All errors are now
    handled and printed to stdout, the loop then continues. The only error
    that is still treated as fatal is a database error.
    
    The approach to finding the expiration date was also too simplistic, as
    the `EncryptionKey()` function fails if the key is already expired.
    Hence add an attempt to find the expiration date manually in this case.
    This is a tricky issue, so the situation where `EncryptionKey()` fails
    but no expiration date can be determined is also logged.

--- contrib/key-expiration/main.go
+++ contrib/key-expiration/main.go
@@ -8,6 +8,7 @@ import (
 
 	"git.sr.ht/~sircmpwn/core-go/config"
 	"github.com/ProtonMail/go-crypto/openpgp"
+	"github.com/ProtonMail/go-crypto/openpgp/packet"
 	_ "github.com/lib/pq"
 )
 
@@ -46,16 +47,37 @@ func main() {
 		}
 		keys, err := openpgp.ReadArmoredKeyRing(strings.NewReader(key))
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Key ID {}: failed to read key material\n", id)
+			continue
 		}
 		entity := keys[0]
+
+		var sig *packet.Signature
+		var pkey *packet.PublicKey
+
 		ekey, found := entity.EncryptionKey(time.Now())
 		if !found {
-			log.Fatal("No public keys suitable for encryption found.")
+			// The key may have already expired, try to find a date manually
+			if entity.PrimaryKey == nil {
+				log.Printf("Key ID {}: no encryption key, no primary key\n", id)
+				continue
+			}
+			ident := entity.PrimaryIdentity()
+			if ident == nil {
+				log.Printf("Key ID {}: no encryption key, no identity found\n", id)
+				continue
+			}
+			if ident.SelfSignature == nil {
+				log.Printf("Key ID {}: no encryption key, identity without self-signature\n", id)
+				continue
+			}
+			pkey = entity.PrimaryKey
+			sig = ident.SelfSignature
+		} else {
+			// We can rely on sig being non-nil and sane if entity.EncryptionKey() did not complain
+			pkey = ekey.PublicKey
+			sig = ekey.SelfSignature
 		}
-		pkey := ekey.PublicKey
-		sig := ekey.SelfSignature
-		// We can rely on sig being non-nil and sane if entity.EncryptionKey() did not complain
 		var expiration *time.Time
 		if sig.KeyLifetimeSecs != nil && *sig.KeyLifetimeSecs != 0 {
 			e := pkey.CreationTime.Add(time.Duration(*sig.KeyLifetimeSecs) * time.Second)
@@ -68,6 +90,8 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
+		} else if !found {
+			log.Printf("Key ID {}: no encryption key, but no expiration date found\n", id)
 		}
 	}
 	log.Println("Done.")
