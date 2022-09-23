b3543db0477f87c2affb048d2853eaff0019fc74
Simon Ser <contact@emersion.fr>
2022-09-23 16:42:24 +0000

    Fix typo in OAuth 2.0 token endpoint error message

--- metasrht/blueprints/oauth2.py
+++ metasrht/blueprints/oauth2.py
@@ -501,7 +501,7 @@ def access_token_POST():
     auth = request.headers.get("Authorization")
     if auth and (client_id or client_secret):
         return access_token_error("invalid_client",
-                "Cannot supply both client_id & client_secret and Authorziation header",
+                "Cannot supply both client_id & client_secret and Authorization header",
                 status=401)
     elif auth:
         parts = auth.split(" ")
