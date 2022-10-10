artoj/meta.sr.ht

commit 5f5d84c02a4c2c3657c38a4c7254d3a3b3547f53

api/graph: verify client secret in issueOAuthGrant
The client secret was passed in but ignored.

The motivation for authenticating the client at this stage is
described in [1]. We only support a single redirect URI and we
don't support refresh tokens yet, so the consequences of missing
this check are not too dangerous.

[1]: https://www.rfc-editor.org/rfc/rfc6749#section-3.2.1

api/graph/model/oauthclient.go
@@ -2,7 +2,10 @@ package model
 
 import (
 	"context"
+	"crypto/subtle"
 	"database/sql"
+	"encoding/base64"
+	"encoding/hex"
 
 	sq "github.com/Masterminds/squirrel"
 
@@ -17,7 +20,8 @@ type OAuthClient struct {
 	Description *string `json:"description"`
 	URL         *string `json:"url"`
 
-	OwnerID int
+	OwnerID          int
+	clientSecretHash string
 
 	alias  string
 	fields *database.ModelFields
@@ -52,9 +56,24 @@ func (o *OAuthClient) Fields() *database.ModelFields {
 			// Always fetch:
 			{"id", "", &o.ID},
 			{"owner_id", "", &o.OwnerID},
+			{"client_secret_hash", "", &o.clientSecretHash},
 		},
 	}
 	return o.fields
+}
+
+func (oc *OAuthClient) VerifyClientSecret(clientSecret string) bool {
+	wantHash, err := hex.DecodeString(oc.clientSecretHash)
+	if err != nil {
+		panic(err)
+	}
+
+	gotHash, err := base64.StdEncoding.DecodeString(clientSecret)
+	if err != nil {
+		return false
+	}
+
+	return subtle.ConstantTimeCompare(wantHash, gotHash) == 1
 }
 
 func (oc *OAuthClient) Query(ctx context.Context, runner sq.BaseRunner,

api/graph/schema.resolvers.go
@@ -1116,6 +1116,10 @@ func (r *mutationResolver) IssueOAuthGrant(ctx context.Context, authorization st
 		panic(err)
 	}
 
+	if !client.VerifyClientSecret(clientSecret) {
+		return nil, fmt.Errorf("invalid client secret")
+	}
+
 	grant := auth.BearerToken{
 		Version:  auth.TokenVersion,
 		Expires:  auth.ToTimestamp(expires),

commit 3cdb44c5fdf70437dea11bd98caa2e07595816e6

graph/model/oauthclient: fix client secret check
We need to hash the client secret before comparing it with the DB
field.

Fixes: 5f5d84c02a4c ("api/graph: verify client secret in issueOAuthGrant")

api/graph/model/oauthclient.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"context"
+	"crypto/sha512"
 	"crypto/subtle"
 	"database/sql"
 	"encoding/base64"
@@ -68,12 +69,13 @@ func (oc *OAuthClient) VerifyClientSecret(clientSecret string) bool {
 		panic(err)
 	}
 
-	gotHash, err := base64.StdEncoding.DecodeString(clientSecret)
+	b, err := base64.StdEncoding.DecodeString(clientSecret)
 	if err != nil {
 		return false
 	}
+	gotHash := sha512.Sum512(b)
 
-	return subtle.ConstantTimeCompare(wantHash, gotHash) == 1
+	return subtle.ConstantTimeCompare(wantHash, gotHash[:]) == 1
 }
 
 func (oc *OAuthClient) Query(ctx context.Context, runner sq.BaseRunner,