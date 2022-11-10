e5b39b978d82b5d8a2a695ec722e7ef030141b2d
Conrad Hoffmann <ch@bitfehler.net>
2022-11-10 11:57:54 +0100

    Remove metasrht.email.send_email
    It was a wrapper around srht.email.send_email that is no longer used.

--- metasrht/email.py
+++ metasrht/email.py
@@ -1,16 +1,5 @@
-import html
-import os
-import srht.email
-from srht.config import cfg, cfgi
 from srht.crypto import internal_anon
 from srht.graphql import exec_gql
-from srht.oauth import current_user
-from string import Template
-
-origin = cfg("meta.sr.ht", "origin")
-owner_name = cfg("sr.ht", "owner-name")
-owner_email = cfg("sr.ht", "owner-email")
-site_name = cfg("sr.ht", "site-name")
 
 def send_email_notification(username, msg):
     email_mutation = """
@@ -20,19 +9,3 @@ def send_email_notification(username, msg):
     """
     r = exec_gql("meta.sr.ht", email_mutation, user=internal_anon,
         username=username, msg=msg)
-
-def send_email(template, *args, encrypt_key=None, headers={}, user=None, **kwargs):
-    if user is None:
-        user = current_user
-    with open(os.path.join(os.path.dirname(__file__), "emails", template)) as f:
-        tmpl = Template(f.read())
-        body = tmpl.substitute(**{
-                'owner_name': owner_name,
-                'owner_email': owner_email,
-                'site_name': site_name,
-                'username': user.username,
-                'user_email': user.email,
-                'root': origin,
-                **kwargs
-            })
-    srht.email.send_email(body, *args, encrypt_key=encrypt_key, **headers)
